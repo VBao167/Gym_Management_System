48bd2e6666bc3726d315a30ca04ca434b8cb2acf
kieungovietbao1672005@gmail.com <kieungovietbao1672005@gmail.com>
2026-07-23 21:09:58 +0700

    cau hinh ket noi SQL Server

--- src/config/settings.py
+++ src/config/settings.py
@@ -74,9 +74,15 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "mssql",
+        "NAME": "GymManagementDB",
+        "HOST": r"IDEAPAD-3\KIEUNGOVIETBAO",
+        "PORT": "",
+        "OPTIONS": {
+            "driver": "ODBC Driver 18 for SQL Server",
+            "extra_params": "TrustServerCertificate=yes",
+        },
     }
 }
 
