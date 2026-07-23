29b597d2b08bdcde6d756f8adbc87f9f675e5dec
kieungovietbao1672005@gmail.com <kieungovietbao1672005@gmail.com>
2026-07-23 21:39:28 +0700

    bao mat Secret Key bang env

--- src/config/settings.py
+++ src/config/settings.py
@@ -10,17 +10,21 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
+import os
 from pathlib import Path
+
+from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+load_dotenv(BASE_DIR.parent / ".env")
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-ra&8+me1*rp(#(&vnn%8uj4x_jy^gd)rjh9y7x8%0j)7#_k98n'
+SECRET_KEY = os.environ["DJANGO_SECRET_KEY"]
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
