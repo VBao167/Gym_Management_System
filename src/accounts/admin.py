2fa3abe4d7d1ac6e912de594c72d5dfa7cb121e8
kieungovietbao1672005@gmail.com <kieungovietbao1672005@gmail.com>
2026-07-23 21:30:27 +0700

    dang ky TaiKhoan tren Django Admin

--- src/accounts/admin.py
+++ src/accounts/admin.py
@@ -1,3 +1,54 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+from .models import TaiKhoan
+
+
+@admin.register(TaiKhoan)
+class TaiKhoanAdmin(UserAdmin):
+    list_display = (
+        "ma_tk",
+        "username",
+        "vai_tro",
+        "is_active",
+        "is_staff",
+        "is_superuser",
+    )
+
+    list_filter = (
+        "vai_tro",
+        "is_active",
+        "is_staff",
+        "is_superuser",
+    )
+
+    search_fields = (
+        "ma_tk",
+        "username",
+    )
+
+    ordering = ("ma_tk",)
+
+    fieldsets = UserAdmin.fieldsets + (
+        (
+            "Thông tin hệ thống Gym",
+            {
+                "fields": (
+                    "ma_tk",
+                    "vai_tro",
+                )
+            },
+        ),
+    )
+
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        (
+            "Thông tin hệ thống Gym",
+            {
+                "fields": (
+                    "ma_tk",
+                    "vai_tro",
+                )
+            },
+        ),
+    )
